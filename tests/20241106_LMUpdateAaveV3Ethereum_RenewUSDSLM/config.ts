fc922d97718946991a03308d3389d2f41052e39d
MartinGbz <martin.grabarz@outlook.fr>
2024-11-06 00:40:17 +0100

    ✨ add proposal

---
+++ tests/20241106_LMUpdateAaveV3Ethereum_RenewUSDSLM/config.ts
@@ -0,0 +1,27 @@
+import {ConfigFile} from '../../generator/types';
+export const config: ConfigFile = {
+  rootOptions: {
+    feature: 'UPDATE_LM',
+    pool: 'AaveV3Ethereum',
+    title: 'Renew USDS LM',
+    shortName: 'RenewUSDSLM',
+    date: '20241106',
+  },
+  poolOptions: {
+    AaveV3Ethereum: {
+      configs: {
+        UPDATE_LM: {
+          emissionsAdmin: '0xac140648435d03f784879cd789130F22Ef588Fcd',
+          rewardToken: 'AaveV3EthereumAssets.USDS_A_TOKEN',
+          rewardTokenDecimals: 18,
+          asset: 'USDS_aToken',
+          distributionEnd: '7',
+          rewardAmount: '159979',
+          whaleAddress: '0x18740A8020dC029B7b8156a7aF8Bd951B65029B0',
+          whaleExpectedReward: '1712.69',
+        },
+      },
+      cache: {blockNumber: 21124604},
+    },
+  },
+};
