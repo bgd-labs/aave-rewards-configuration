d00bdd57af004a35ef34a0c66f0bad640c95df50
MartinGbz <martin.grabarz@outlook.fr>
2024-11-06 02:12:50 +0100

    🔧 update to 8% delta

--- tests/utils/LMBaseTest.sol
+++ tests/utils/LMBaseTest.sol
@@ -74,7 +74,7 @@ abstract contract LMBaseTest is Test {
     assertApproxEqRel(
       balanceAfter - balanceBefore,
       expectedReward, // Approx estimated rewards with current emissions
-      0.05e18, // 5% delta
+      0.08e18, // 8% delta
       'Invalid delta on claimed rewards'
     );
 
