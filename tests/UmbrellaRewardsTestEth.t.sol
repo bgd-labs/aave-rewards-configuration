fd861c5c217f77dddd39122df89d7ccd694407dc
Harsh Pandey <harshsatishpandey@gmail.com>
2025-06-06 15:46:14 +0400

    fix: eth umbrella reward example

--- tests/UmbrellaRewardsTestEth.t.sol
+++ tests/UmbrellaRewardsTestEth.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
 import {UmbrellaRewardsBaseTest} from './utils/UmbrellaRewardsBaseTest.sol';
-import {UmbrellaEthereum} from 'aave-address-book/UmbrellaEthereum.sol';
+import {UmbrellaEthereumAssets} from 'aave-address-book/UmbrellaEthereum.sol';
 import {UmbrellaEthereumConfig} from './utils/networkConfig/UmbrellaEthereumConfig.sol';
 import {EngineFlags} from 'aave-v3-origin/contracts/extensions/v3-config-engine/EngineFlags.sol';
 
@@ -14,14 +14,13 @@ contract UmbrellaRewardsTestEth is UmbrellaRewardsBaseTest, UmbrellaEthereumConf
   function configureUpdates() public pure override returns (RewardConfig[] memory) {
     RewardConfig[] memory newConfig = new RewardConfig[](1);
     newConfig[0] = RewardConfig({
-      asset: address(0), // STK_WA_WETH (not deployed yet)
+      asset: UmbrellaEthereumAssets.STK_WA_WETH_V1,
       reward: AaveV3EthereumAssets.WETH_A_TOKEN,
       rewardPayer: address(AaveV3Ethereum.COLLECTOR),
       maxEmissionPerSecond: uint256(400 * 1e18) / 365 days,
       distributionEnd: EngineFlags.KEEP_CURRENT
     });
 
-    // commented as we don't want ci to fail
-    // return newConfig;
+    return newConfig;
   }
 }
